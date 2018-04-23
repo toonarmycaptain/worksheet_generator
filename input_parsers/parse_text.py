802318e7f869b3b31b426222b930f6de87f6b5f9
toonarmycaptain <toonarmycaptain@hotmail.com>
2018-04-23 11:12:34 -0500

    Fixes Issue #2 Changed naming to refer to solution rather than correct_answers
    Signed-off-by: toonarmycaptain <toonarmycaptain@hotmail.com>

--- input_parsers/parse_text.py
+++ input_parsers/parse_text.py
@@ -1,7 +1,7 @@
 """
 Parse text file in line format:
 Question 1///Answer 1a///Answer 1b///Answer 1c///Answer 1d***Answer1***Answer2***
-    into tuples: question, tuple_of_answer_choices, tuple_of_correct_answers
+    into tuples: question, tuple_of_answer_choices, tuple_of_solutions
 
 Tuples can then be passed into a formatting or storage format:
 
@@ -11,14 +11,16 @@ for question_text, question_answers in parse_text_file(text_file_name):
             -dict form could use iteration through keys to output
         ->store in database or object
 """
+
+
 def parse_text_file(question_filename):
     """
     Takes questions from lines of a text file with the form:
     Question 1///Answer 1a///Answer 1b///Answer 1c***correct***ans***
-        and yields question string, tuples of answers and correct answers
+        and yields question string, tuples of answers and solution
 
     :param question_filename: filepath\filename of text file
-    :yield: str, tuple, tuple question, tuple_of_answers, tuple_correct_answers
+    :yield: str, tuple, tuple question, tuple_of_answers, tuple_of_solutions
     """
     with open(question_filename) as f:
         for line in f.readlines():
@@ -27,23 +29,22 @@ def parse_text_file(question_filename):
             if line == '':
                 continue
 
-
             if '***' in line:
-                answer_sep = line.split('***')
-                answers = tuple([x.strip() for x in answer_sep[1:] if x != ''])
+                solution_sep = line.split('***')
+                solution = tuple([x.strip() for x in solution_sep[1:] if x != ''])
             else:
-                answers = None
+                solution = None
 
             q_and_a = line.split('///')
             # q_and_a[0] is question, q_and_a[-1] is answers
             question = q_and_a[0].strip(' ')
             answer_choices = tuple([x.strip() for x in q_and_a[1:-1] if x != ''])
 
-            yield question, answer_choices, answers
+            yield question, answer_choices, solution
 
 
 if __name__ == '__main__':
 
     text_file_name = input('Path_to_text_file\\filename: ')
-    for question_text, answer_choices, answers in parse_text_file(text_file_name):
-        print(f'Question: {question_text}\nAnswer choices: {answer_choices}\nCorrect answer/s: {answers}')
+    for question_text, answer_choices, solution in parse_text_file(text_file_name):
+        print(f'Question: {question_text}\nAnswer choices: {answer_choices}\n Solution: {solution}')
--- question_class.py
+++ question_class.py
@@ -2,24 +2,30 @@
 Question classes
 
 """
-class Question():
+
+
+class Question:
     pass
+
 
 class TextQ(Question):
     """
     Question class with question and answers solely composed of text.
     """
-    def __init__(self, question_text: str, answer_tuple: tuple, correct_ans: tuple=None):
+    def __init__(self,
+                 question_text: str,
+                 answers: tuple,
+                 solution: tuple=None):
         question_text = question_text
-        answers = answer_tuple
-        num_answers = len(answer_tuple) + 1 # for use with 'in range(num_answers)
+        answers = answers
+        num_answers = len(answers) + 1  # for use with 'in range(num_answers)
         # for more than one answer, use tuple, or slice? This will output a list
         # eg answers = [1, 2, 3, 4] => correct = [1:4:2] => correct >>> [2, 4]
-        correct_answer = correct_ans
+        solution = solution
 
-        # assert all correct answers in answers
-        if correct_answer is not None:
-            assert set(correct_answer) & set(answers) == set(correct_ans),\
+        # assert all solutions  are in the answer choices
+        if solution is not None:
+            assert set(solution) & set(answers) == set(solution),\
                 "Answer not in answer choices."
 
 
