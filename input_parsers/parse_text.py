e97ff3aa112f1c6e9abf3bbd32ff5abdf00d218a
toonarmycaptain <toonarmycaptain@hotmail.com>
2018-04-24 11:20:38 -0500

    Refactor to extract functions.
    Extracted function to parse each non-blank/question line.
    Extracted function to parse question line for solutions.
    Extracted function to parse question line for question text, answers.
    
    Documented new functions.
    
    Signed-off-by: toonarmycaptain <toonarmycaptain@hotmail.com>

--- input_parsers/parse_text.py
+++ input_parsers/parse_text.py
@@ -13,13 +13,57 @@ for question_text, question_answers in parse_text_file(text_file_name):
 """
 
 
+def extract_q_and_a(line: str):
+    """
+    Takes question string (line from file), returns question text as a string
+    and a tuple of answer choices.
+
+    :param line: str
+    :return: str, tuple: question, answer_choices
+    """
+    q_and_a = line.split('///')
+    # q_and_a[0] is question, q_and_a[-1] is answers
+    question = q_and_a[0].strip(' ')
+    answer_choices = tuple([x.strip() for x in q_and_a[1:-1] if x != ''])
+    return question, answer_choices
+
+
+def extract_solution(line: str):
+    """
+    Takes question string (line from file), returns solution/s as tuple.
+
+    :param line: str
+    :return: tuple
+    """
+    if '***' in line:
+        solution_sep = line.split('***')
+        return tuple([x.strip() for x in solution_sep[1:] if x != ''])
+    else:
+        return None
+
+
+def parse_question(line: str):
+    """
+    Takes line of a text file with the form:
+    Question 1///Answer 1a///Answer 1b///Answer 1c***correct***ans***
+    and returns question string, tuples of answers and solution
+
+    :param line: line of text file containing question text
+    :return: str, tuple, tuple: question, tuple_answers, tuple_solutions or None
+    """
+    question, answer_choices = extract_q_and_a(line)
+    solution = extract_solution(line)
+
+    return question, answer_choices, solution
+
+
 def parse_text_file(question_filename):
     """
-    Takes questions from lines of a text file with the form:
-    Question 1///Answer 1a///Answer 1b///Answer 1c***correct***ans***
+    Takes questions from none-empty lines of a text file with the form:
+    Question 1///Answer 1a///Answer 1b///Answer 1c***correct***ans***etc
         and yields question string, tuples of answers and solution
 
-    :param question_filename: filepath\filename of text file
+    :param question_filename: filepath/filename of text file
     :yield: str, tuple, tuple question, tuple_of_answers, tuple_of_solutions
     """
     with open(question_filename) as f:
@@ -28,18 +72,7 @@ def parse_text_file(question_filename):
                 line = line[:-1]
             if line == '':
                 continue
-
-            if '***' in line:
-                solution_sep = line.split('***')
-                solution = tuple([x.strip() for x in solution_sep[1:] if x != ''])
-            else:
-                solution = None
-
-            q_and_a = line.split('///')
-            # q_and_a[0] is question, q_and_a[-1] is answers
-            question = q_and_a[0].strip(' ')
-            answer_choices = tuple([x.strip() for x in q_and_a[1:-1] if x != ''])
-
+            question, answer_choices, solution = parse_question(line)
             yield question, answer_choices, solution
 
 
@@ -47,4 +80,4 @@ if __name__ == '__main__':
 
     text_file_name = input('Path_to_text_file\\filename: ')
     for question_text, answer_choices, solution in parse_text_file(text_file_name):
-        print(f'Question: {question_text}\nAnswer choices: {answer_choices}\n Solution: {solution}')
+        print(f'Question: {question_text}\nAnswer choices: {answer_choices}\n Solution: {solution}\n')
