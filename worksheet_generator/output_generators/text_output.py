979ad1d8d16327b7e536e8f2d4fd40534d87baab
David <toonarmycaptain@hotmail.com>
2018-05-04 14:43:15 -0500

    Encapsulated text file generation in function, complete with some formatting parameters, optional save location.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- worksheet_generator/output_generators/text_output.py
+++ worksheet_generator/output_generators/text_output.py
@@ -1,5 +1,5 @@
 """
-Base text output format:
+Generic/default text output format:
 
 header information (eg author etc)
 Title of worksheet
@@ -13,28 +13,56 @@ import os
 
 from worksheet_generator.input_parsers import parse_text
 
+_output_repository = '..\\..\\generated_worksheets'
 
+
+def generate_text_worksheet(output_filename: str, questions: list, target_path=_output_repository):
+    """
+    Take filename, question list, path to save file at, generate a text file at
+    specified path.
+    Return path of created text file.
+
+    :param output_filename: str
+    :param questions: list of TextQ question objects
+    :param target_path: str
+    :return output_path: str
+    """
+    os.makedirs(target_path, exist_ok=True)
+
+    rel_path = target_path
+    output_path = os.path.join(rel_path, output_filename + '.txt')
+
+    # TODO: error handling if file already exists
+
+    spacing = '\n'
+
+    question_answer_spacing = 1 * spacing  # blank lines
+    question_question_spacing = 2 * spacing  # blank lines
+
+    with open(output_path, 'w') as output_file:
+        for question in questions:
+            output_file.write(f'Question {questions.index(question) + 1}: {question.question_text}')
+            output_file.write(question_answer_spacing)
+            if question.answers:
+                for answer in question.answers:
+                    output_file.write(f'Answer {question.answers.index(answer) + 1}: {answer} ')
+                output_file.write(spacing)  # blank line terminating answers
+            output_file.write(question_question_spacing)
+
+    return output_path
 
 
 if __name__ == '__main__':
 
-    rel_path = '..\\..\\generated_worksheets'
+    generated_worksheets_folder = '..\\..\\generated_worksheets'
 
     input_filename = input('Please input path of text file to parse: ')
 
-    output_filename = input('Please select output filename: ')
-
-    output_path = os.path.join(rel_path, output_filename)
+    test_filename = input('Please select output filename: ')
 
     output_questions = [x for x in parse_text.generate_questions(input_filename)]
 
-    with open(output_path, 'w') as output_file:
-
-        for question in output_questions:
-            output_file.write(f'Question {output_questions.index(question) + 1}: {question.question_text}')
-            output_file.write('\n')
-            if question.answers:
-                for answer in question.answers:
-                    output_file.write(f'Answer {question.answers.index(answer) + 1}: {answer} ')
-                output_file.write('\n')
-            output_file.write('\n\n')
+    generate_text_worksheet(test_filename,
+                            output_questions,
+                            # generated_worksheets_folder+'_chicken' <- additional param tested working.
+                            )
