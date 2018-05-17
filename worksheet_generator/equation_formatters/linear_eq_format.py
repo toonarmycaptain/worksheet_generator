62f588e4f7a902e3b8fc8f3c13c824a3536780f5
David <toonarmycaptain@hotmail.com>
2018-05-17 18:54:05 -0500

    Combined if..else into f-string.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- worksheet_generator/equation_formatters/linear_eq_format.py
+++ worksheet_generator/equation_formatters/linear_eq_format.py
@@ -84,12 +84,9 @@ def fraction_format_without_sign(numerator: int, denominator: int,
         if num_var or denom_var:  # 1/x or x
             return f"{num_var if num_var else '1'}{f'/{denom_var}' if denom_var else ''}"
         return '1'
-    else:
-        # numerator
-        if num_var:
-            fraction_str = f"{abs(numerator) if abs(numerator) != 1 else ''}{num_var}"
-        else:
-            fraction_str = f"{abs(numerator)}"
+    else:  # numerator/denominator != 1 ie is a fraction.
+        fraction_str = f'''{f"{abs(numerator) if abs(numerator) != 1 else ''}{num_var}" if num_var  else abs(numerator)}'''
+
         # denominator
         if denominator != 1:
             return fraction_str + f"/{abs(denominator)}{denom_var if denom_var else ''}"
