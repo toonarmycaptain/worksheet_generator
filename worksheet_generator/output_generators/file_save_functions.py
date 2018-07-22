4492d4a7fab457b78c54cc6e4463aac6c905113a
David <toonarmycaptain@hotmail.com>
2018-07-22 15:05:06 -0500

    Add type hints, skeleton functions.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- worksheet_generator/output_generators/file_save_functions.py
+++ worksheet_generator/output_generators/file_save_functions.py
@@ -3,7 +3,7 @@ Helper functions for saving worksheets and files.
 """
 
 
-def check_add_file_extension(filename, file_format='.pdf'):
+def check_add_file_extension(filename: str, file_format: str='.pdf'):
     """
     Checks whether entered filename contains necessary extension, and adds
     appropriate extension if needed.
@@ -14,3 +14,21 @@ def check_add_file_extension(filename, file_format='.pdf'):
     """
     if filename[:4].lower() is not file_format:  # check period? Probably unnecessary if file_format not user-input
         return filename + file_format
+
+
+def check_file_path_name():
+    # check filepath and filename for existence/nonexistence.
+    if check_filepath():
+        if check_filename():
+            return True
+    return False
+
+
+def check_filepath():
+    # check file path for existence, create if doesn't exist?
+
+    pass
+
+def check_filename():
+    #check filename for existence
+    return not filename_exists
