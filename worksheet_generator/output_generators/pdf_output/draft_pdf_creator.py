66ea01754d06f25f62676e672fbdb6fee645018b
David <toonarmycaptain@hotmail.com>
2018-07-22 14:58:20 -0500

    Rearrange, flesh out draft workflow, add TODOs.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- worksheet_generator/output_generators/pdf_output/draft_pdf_creator.py
+++ worksheet_generator/output_generators/pdf_output/draft_pdf_creator.py
@@ -7,20 +7,26 @@ if __name__ == '__main__':
     import worksheet_generator.question_class as question_class
     import worksheet_generator.input_parsers.parse_text as parse_text
     from worksheet_generator.output_generators.pdf_output.abc_ord import a_to_z as ordinal_abc
-    env = Environment(
-        loader=FileSystemLoader('./PDF_templates'), autoescape=True
-        )
-    template = env.get_template("draft_single_column_pdf.jinja2.template")
+
 
     # read test_test_questions
     worksheet_q_list = []
-    text_file_name = '..//..//..//worksheet_generator/input_parsers/test_text_questions.txt'   # rel path to any source, or copy user source into some project folder/section of database
+
+    text_file_name = '..//..//..//worksheet_generator/input_parsers/test_text_questions.txt'
     for question_text, answer_choices, solution in parse_text.parse_text_file(text_file_name):
         worksheet_q_list.append(question_class.TextQ(question_text, answer_choices, solution))
 
+    env = Environment(
+        loader=FileSystemLoader('./PDF_templates'), autoescape=True
+        )
+
+    template = env.get_template("draft_single_column_pdf.jinja2.template")  # TODO: factor template fetch into function
+
+    # TODO: factor template dict into funct, possibly drawing needed data based on chosen template
     template_vars = {"title": "Test worksheet",
                      "question_list": worksheet_q_list,
-                     "ordinal_abc": ordinal_abc}
+                     "ordinal_abc": ordinal_abc
+                     }
 
 # render html of these questions
 
