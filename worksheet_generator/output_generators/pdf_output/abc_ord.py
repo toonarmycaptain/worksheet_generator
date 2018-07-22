6b6cf6bba8950fe1fb1e2cde93bc171f6a314644
David <toonarmycaptain@hotmail.com>
2018-07-22 15:48:32 -0500

    Add module docstring.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- worksheet_generator/output_generators/pdf_output/abc_ord.py
+++ worksheet_generator/output_generators/pdf_output/abc_ord.py
@@ -1,3 +1,8 @@
+"""
+Helper functions for producing letter sequences for questions/answers.
+"""
+
+
 a_to_z = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
           'w', 'x', 'y', 'z']
 
